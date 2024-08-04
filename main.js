31c6d823e3fa3028a1ef41f2a2ead60afb97696d
alan garcia mendaña <alitan31@gmail.com>
2024-08-04 17:40:06 -0300

    agregado de PDF

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const github = document.getElementById("github")
 
 github.addEventListener("click",()=>{window.open('https://github.com/alangarciamendana', '_blank')})
+
+const CV = document.getElementById("cv")
+CV.addEventListener("click",()=>{window.open('https://drive.google.com/file/d/15lhfD4NenvCB_v7zdxPxCmMn91QxQO13/view?usp=drive_link', '_blank')})
